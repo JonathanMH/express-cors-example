f4401c9669eec5726560c81d01a2005436dca8be
Jonathan M. Hethey <public@jonathanmh.com>
2015-06-18 13:20:43 +0200

    initial

---
+++ app.js
@@ -0,0 +1,32 @@
+var express = require('express');
+
+var app = express();
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get('/', function (req, res) {
+  var data = {
+    "bestAnimals": [
+      "wombat",
+      "corgi",
+      "puffer fish",
+      "owl",
+      "crow"
+    ]
+  };
+
+  res.json(data);
+});
+
+
+var server = app.listen(3000, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+
+});
